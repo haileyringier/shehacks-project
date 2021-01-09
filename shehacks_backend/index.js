05e7f1f49e70aa1d0b20cbb07ff25c78ed598acb
Joanne <joanne.zhu@mail.utoronto.ca>
2021-01-09 16:46:28 -0500

    added server and data

---
+++ shehacks_backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+var questionsJson = require("../shehacks_backend/questions.json");
+
+//app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.json(questionsJson[0]);
+})
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log("connected"));
