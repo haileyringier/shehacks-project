e866bfde77ae120bbd7e6ad034cdf80268b7ac8e
nikadari <nikadariani@gmail.com>
2021-01-09 21:27:24 -0500

    attempt to render one question using hooks

--- shehacks_frontend/src/Components/Question.js
+++ shehacks_frontend/src/Components/Question.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 export default function Question(props){
+    
     console.log(props)
+    
     const clickedAnswer = (event) => {
-
+        
         console.log(event.target)
+
     }
 
+    
+
     const renderQuestions = () => props.questions.map(q => {
+        const [count, setCount] = useState(0);
         return (
             <li>
                 <h3>{q.number} .  {q.question}</h3>
-                <button className="question-option" onClick={clickedAnswer}>A. {q.optionA}</button> 
+                <button className="question-option" onClick={() => setCount(count + 1)}>A. {q.optionA}</button> 
                 <button className="question-option" onClick={clickedAnswer}>B. {q.optionB}</button>
                 <button className="question-option" onClick={clickedAnswer}>C. {q.optionC}</button>
                 <button className="question-option" onClick={clickedAnswer}>D. {q.optionD}</button>
+                console.log(event.target)
             </li>
         )
     })
